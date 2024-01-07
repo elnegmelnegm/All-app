8fdd8dc17cbc90e21c781d56c8149aede2184eff
elnegmelnegm <48047676+elnegmelnegm@users.noreply.github.com>
2024-01-07 19:34:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ st.markdown('''
 st.markdown('''
 Powered by Google AI <img src="https://seeklogo.com/images/G/google-ai-logo-996E85F6FD-seeklogo.com.png" width="20" height="20"> Streamlit <img src="https://global.discourse-cdn.com/business7/uploads/streamlit/original/2X/f/f0d0d26db1f2d99da8472951c60e5a1b782eb6fe.png" width="22" height="22"> Python <img src="https://i.ibb.co/wwCs096/nn-1-removebg-preview-removebg-preview.png" width="22" height="22">''', unsafe_allow_html=True)
 
-
+st.link_button("Go to Drugs side effects and drug drug interactions", "https://drug-drug-5qohebfrcptarhcj8niwgo.streamlit.app/")
