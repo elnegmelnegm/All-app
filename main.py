45ede74d65e84c62b5c113bef66efcc667170397
elnegmelnegm <48047676+elnegmelnegm@users.noreply.github.com>
2024-01-07 19:27:36 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+# Display header
+st.set_page_config(
+    page_title="ُEDA AI Chat",
+    page_icon="https://www.edaegypt.gov.eg/media/wc3lsydo/group-287.png",
+    layout="wide",
+)
+
+st.markdown('''
+<img src="https://www.edaegypt.gov.eg/media/wc3lsydo/group-287.png" width="250" height="100">''', unsafe_allow_html=True)
+
+st.markdown('''
+Powered by Google AI <img src="https://seeklogo.com/images/G/google-ai-logo-996E85F6FD-seeklogo.com.png" width="20" height="20"> Streamlit <img src="https://global.discourse-cdn.com/business7/uploads/streamlit/original/2X/f/f0d0d26db1f2d99da8472951c60e5a1b782eb6fe.png" width="22" height="22"> Python <img src="https://i.ibb.co/wwCs096/nn-1-removebg-preview-removebg-preview.png" width="22" height="22">''', unsafe_allow_html=True)
+
+
