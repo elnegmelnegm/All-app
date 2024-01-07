32c78e9144c935a14699665a59dc9465c717e1a8
elnegmelnegm <48047676+elnegmelnegm@users.noreply.github.com>
2024-01-07 19:53:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ st.markdown('''
 st.markdown('''
 Powered by Google AI <img src="https://seeklogo.com/images/G/google-ai-logo-996E85F6FD-seeklogo.com.png" width="20" height="20"> Streamlit <img src="https://global.discourse-cdn.com/business7/uploads/streamlit/original/2X/f/f0d0d26db1f2d99da8472951c60e5a1b782eb6fe.png" width="22" height="22"> Python <img src="https://i.ibb.co/wwCs096/nn-1-removebg-preview-removebg-preview.png" width="22" height="22">''', unsafe_allow_html=True)
 
-st.link_button("Drug side effects and interactions", "https://drug-drug-5qohebfrcptarhcj8niwgo.streamlit.app/")
+st.link_button("Drug side effects and interactions", "https://drug-drug-5qohebfrcptarhcj8niwgo.streamlit.app/",help="Press the button")
 st.markdown('''
 This application enables users to access comprehensive information about a specific drug by providing its name. Users can input the name of the drug, and the app will retrieve detailed information on various aspects, including but not limited to, indications, contraindications, potential side effects, and any known drug interactions.''', unsafe_allow_html=True)
 
